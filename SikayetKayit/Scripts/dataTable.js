80a7d49850dbe9c3820a20dcdaeabc262d317a4f
arenka <arifeneskarahan@hotmail.com>
2020-01-03 02:44:10 +0300

    .

---
+++ SikayetKayit/Scripts/dataTable.js
@@ -0,0 +1,9 @@
+﻿$('#table').dataTable({
+    pageLength: 10,
+    lengthMenu: [[10, 25, 50, -1], ["10 Adet", "25 Adet", "50 Adet", "Tümü"]],
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.20/i18n/Turkish.json'
+    }
+});
+
+
